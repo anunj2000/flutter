88fa49a48cd06c613dbf88c0c1f44d097e866121
Anu N J <anunj2000@gmail.com>
2025-01-05 21:53:02 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,522 @@
+import 'package:flutter/material.dart';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:connectivity_plus/connectivity_plus.dart';
+
+void main() {
+  runApp(const MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: MyApp(),
+  ));
+}
+
+class Config {
+  static const String usersApiUrl = 'https://gorest.co.in/public/v1/users';
+  static const String createUserApiUrl = 'https://gorest.co.in/public/v1/users';
+  static const String apiToken =
+      '3585567e74f1bf6ef98fb79143f8c739202db08397741988a0b7fc628f64c101'; // Replace with your token
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  List<dynamic> data = [];
+
+  @override
+  void initState() {
+    super.initState();
+    fetchData();
+  }
+
+  Future<void> fetchData() async {
+    var connectivityResult = await Connectivity().checkConnectivity();
+    if (connectivityResult == ConnectivityResult.none) {
+      _showNoConnectionDialog();
+      return;
+    }
+
+    try {
+      final res = await http.get(Uri.parse(Config.usersApiUrl), headers: {
+        "Authorization": "Bearer ${Config.apiToken}",
+      });
+
+      if (res.statusCode == 200) {
+        var jsonData = jsonDecode(res.body);
+
+        if (jsonData is Map && jsonData.containsKey('data')) {
+          setState(() {
+            data = jsonData['data'];
+          });
+        } else {
+          throw Exception('Unexpected JSON format: ${jsonData.runtimeType}');
+        }
+      } else {
+        throw Exception(
+            'Failed to load data. Status code: ${res.statusCode}. Body: ${res.body}');
+      }
+    } catch (e) {
+      print('Error fetching data: $e');
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Error fetching data: $e')),
+      );
+    }
+  }
+
+  void _showNoConnectionDialog() {
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        title: const Text('Network Connection Issue'),
+        content: const Text('No internet connection. Please try again later.'),
+        actions: [
+          TextButton(
+            onPressed: () => Navigator.of(context).pop(),
+            child: const Text('OK'),
+          ),
+        ],
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: const Color(0xFF2979FF),
+        title: const Text("User List"),
+      ),
+      body: LayoutBuilder(
+        builder: (context, constraints) {
+          if (constraints.maxWidth > 600) {
+            return GridView.builder(
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 3,
+                crossAxisSpacing: 10,
+                mainAxisSpacing: 10,
+              ),
+              itemCount: data.length,
+              itemBuilder: (context, index) {
+                String name = data[index]['name'];
+                return GestureDetector(
+                  onTap: () async {
+                    bool? isUpdated = await Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) =>
+                            UserDetailScreen(user: data[index]),
+                      ),
+                    );
+                    if (isUpdated == true) {
+                      fetchData(); // Refresh the data if updated
+                    }
+                  },
+                  child: Card(
+                    color: Colors.blue[100],
+                    child: Center(
+                      child: Text(name, style: const TextStyle(fontSize: 18)),
+                    ),
+                  ),
+                );
+              },
+            );
+          } else {
+            return ListView.builder(
+              itemCount: data.length,
+              itemBuilder: (context, index) {
+                String name = data[index]['name'];
+                return ListTile(
+                  title: Text(name, style: const TextStyle(fontSize: 18)),
+                  trailing: IconButton(
+                    icon: const Icon(Icons.edit),
+                    onPressed: () async {
+                      bool? isUpdated = await Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) =>
+                              UserDetailScreen(user: data[index]),
+                        ),
+                      );
+                      if (isUpdated == true) {
+                        fetchData(); // Refresh the data if updated
+                      }
+                    },
+                  ),
+                  tileColor: Colors.blue[50],
+                  contentPadding:
+                      const EdgeInsets.symmetric(vertical: 10, horizontal: 16),
+                );
+              },
+            );
+          }
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => const NewUserForm()),
+          );
+        },
+        backgroundColor: Colors.blue,
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
+
+class NewUserForm extends StatefulWidget {
+  const NewUserForm({super.key});
+
+  @override
+  _NewUserFormState createState() => _NewUserFormState();
+}
+
+class _NewUserFormState extends State<NewUserForm> {
+  final _formKey = GlobalKey<FormState>();
+  final TextEditingController _nameController = TextEditingController();
+  final TextEditingController _emailController = TextEditingController();
+  String _selectedGender = 'male'; // Default gender selection
+  bool _isActive = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _loadFormData();
+  }
+
+  Future<void> _loadFormData() async {
+    final prefs = await SharedPreferences.getInstance();
+    setState(() {
+      _nameController.text = prefs.getString('newUserName') ?? '';
+      _emailController.text = prefs.getString('newUserEmail') ?? '';
+      _selectedGender =
+          prefs.getString('newUserGender') ?? 'male'; // Load gender
+      _isActive = prefs.getBool('newUserIsActive') ?? false;
+    });
+  }
+
+  Future<void> _saveFormData() async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setString('newUserName', _nameController.text);
+    await prefs.setString('newUserEmail', _emailController.text);
+    await prefs.setString('newUserGender', _selectedGender);
+    await prefs.setBool('newUserIsActive', _isActive);
+  }
+
+  Future<void> _submitForm() async {
+    var connectivityResult = await Connectivity().checkConnectivity();
+    if (connectivityResult == ConnectivityResult.none) {
+      _showNoConnectionDialog();
+      return;
+    }
+
+    final newUser = {
+      "name": _nameController.text,
+      "email": _emailController.text,
+      "gender": _selectedGender,
+      "status": _isActive ? 'active' : 'inactive',
+    };
+
+    try {
+      final response = await http.post(
+        Uri.parse(Config.createUserApiUrl),
+        headers: {
+          "Content-Type": "application/json",
+          "Authorization": "Bearer ${Config.apiToken}",
+        },
+        body: jsonEncode(newUser),
+      );
+
+      if (response.statusCode == 201) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(content: Text('User added successfully!')),
+        );
+        Navigator.pop(context, true); // Return true to indicate success
+      } else {
+        var responseJson = jsonDecode(response.body);
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(
+              content: Text('Failed to add user: ${responseJson['message']}')),
+        );
+      }
+    } catch (e) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Error adding user: $e')),
+      );
+    }
+  }
+
+  void _showNoConnectionDialog() {
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        title: const Text('Network Connection Issue'),
+        content: const Text('No internet connection. Please try again later.'),
+        actions: [
+          TextButton(
+            onPressed: () => Navigator.of(context).pop(),
+            child: const Text('OK'),
+          ),
+        ],
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: const Color(0xFF2979FF),
+        title: const Text("New User"),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              const Text(
+                'Add New User',
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(height: 20),
+              TextFormField(
+                controller: _nameController,
+                decoration: const InputDecoration(
+                  labelText: 'Name',
+                  border: OutlineInputBorder(),
+                ),
+              ),
+              const SizedBox(height: 20),
+              TextFormField(
+                controller: _emailController,
+                decoration: const InputDecoration(
+                  labelText: 'Email',
+                  border: OutlineInputBorder(),
+                ),
+              ),
+              const SizedBox(height: 20),
+              DropdownButtonFormField<String>(
+                value: _selectedGender,
+                items: const [
+                  DropdownMenuItem(
+                    value: 'male',
+                    child: Text('Male'),
+                  ),
+                  DropdownMenuItem(
+                    value: 'female',
+                    child: Text('Female'),
+                  ),
+                  DropdownMenuItem(
+                    value: 'other',
+                    child: Text('Other'),
+                  ),
+                ],
+                onChanged: (value) {
+                  setState(() {
+                    _selectedGender = value!;
+                  });
+                },
+                decoration: const InputDecoration(
+                  labelText: 'Gender',
+                  border: OutlineInputBorder(),
+                ),
+              ),
+              const SizedBox(height: 20),
+              SwitchListTile(
+                title: const Text('Active'),
+                value: _isActive,
+                onChanged: (bool value) {
+                  setState(() {
+                    _isActive = value;
+                  });
+                },
+              ),
+              const SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: _submitForm,
+                child: const Text('Submit'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class UserDetailScreen extends StatefulWidget {
+  final dynamic user;
+
+  const UserDetailScreen({super.key, required this.user});
+
+  @override
+  _UserDetailScreenState createState() => _UserDetailScreenState();
+}
+
+class _UserDetailScreenState extends State<UserDetailScreen> {
+  final _formKey = GlobalKey<FormState>();
+  late TextEditingController _nameController;
+  late TextEditingController _emailController;
+  late String _selectedGender;
+  late bool _isActive;
+
+  @override
+  void initState() {
+    super.initState();
+    _nameController = TextEditingController(text: widget.user['name']);
+    _emailController = TextEditingController(text: widget.user['email']);
+    _selectedGender = widget.user['gender'];
+    _isActive = widget.user['status'] == 'active';
+  }
+
+  Future<void> _updateUser() async {
+    var connectivityResult = await Connectivity().checkConnectivity();
+    if (connectivityResult == ConnectivityResult.none) {
+      _showNoConnectionDialog();
+      return;
+    }
+
+    final updatedUser = {
+      "name": _nameController.text,
+      "email": _emailController.text,
+      "gender": _selectedGender,
+      "status": _isActive ? 'active' : 'inactive',
+    };
+
+    try {
+      final response = await http.put(
+        Uri.parse('https://gorest.co.in/public/v1/users/${widget.user['id']}'),
+        headers: {
+          "Content-Type": "application/json",
+          "Authorization": "Bearer ${Config.apiToken}",
+        },
+        body: jsonEncode(updatedUser),
+      );
+
+      if (response.statusCode == 200) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(content: Text('User updated successfully!')),
+        );
+        Navigator.pop(context, true); // Return true to indicate success
+      } else {
+        var responseJson = jsonDecode(response.body);
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(
+              content:
+                  Text('Failed to update user: ${responseJson['message']}')),
+        );
+      }
+    } catch (e) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Error updating user: $e')),
+      );
+    }
+  }
+
+  void _showNoConnectionDialog() {
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        title: const Text('Network Connection Issue'),
+        content: const Text('No internet connection. Please try again later.'),
+        actions: [
+          TextButton(
+            onPressed: () => Navigator.of(context).pop(),
+            child: const Text('OK'),
+          ),
+        ],
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: const Color(0xFF2979FF),
+        title: const Text("Edit User"),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              const Text(
+                'Edit User Details',
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(height: 20),
+              TextFormField(
+                controller: _nameController,
+                decoration: const InputDecoration(
+                  labelText: 'Name',
+                  border: OutlineInputBorder(),
+                ),
+              ),
+              const SizedBox(height: 20),
+              TextFormField(
+                controller: _emailController,
+                decoration: const InputDecoration(
+                  labelText: 'Email',
+                  border: OutlineInputBorder(),
+                ),
+              ),
+              const SizedBox(height: 20),
+              DropdownButtonFormField<String>(
+                value: _selectedGender,
+                items: const [
+                  DropdownMenuItem(
+                    value: 'male',
+                    child: Text('Male'),
+                  ),
+                  DropdownMenuItem(
+                    value: 'female',
+                    child: Text('Female'),
+                  ),
+                  DropdownMenuItem(
+                    value: 'other',
+                    child: Text('Other'),
+                  ),
+                ],
+                onChanged: (value) {
+                  setState(() {
+                    _selectedGender = value!;
+                  });
+                },
+                decoration: const InputDecoration(
+                  labelText: 'Gender',
+                  border: OutlineInputBorder(),
+                ),
+              ),
+              const SizedBox(height: 20),
+              SwitchListTile(
+                title: const Text('Active'),
+                value: _isActive,
+                onChanged: (bool value) {
+                  setState(() {
+                    _isActive = value;
+                  });
+                },
+              ),
+              const SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: _updateUser,
+                child: const Text('Save Changes'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
